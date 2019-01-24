c114d3141e4c36fbc806ad81cdddba0f77a30945
ducaifeng86 <1613195189@qq.com>
2019-01-24 11:47:08 +0800

    底部导航的实现

--- src/pages/category/category.ts
+++ src/pages/category/category.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+  selector: 'page-category',
+  templateUrl: 'category.html'
 })
-export class AboutPage {
+export class CategoryPage {
 
   constructor(public navCtrl: NavController) {
 
