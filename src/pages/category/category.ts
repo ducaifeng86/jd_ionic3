4549a724eaa7a7a127640ca733f8c31a76219bc1
ducaifeng86 <1613195189@qq.com>
2019-01-25 10:37:36 +0800

    分类页面动态请求数据的实现

--- src/pages/category/category.ts
+++ src/pages/category/category.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { ConfigProvider } from '../../providers/config/config';
+import { HttpServicesProvider } from '../../providers/http-services/http-services';
 @Component({
   selector: 'page-category',
   templateUrl: 'category.html'
 })
 export class CategoryPage {
-
-  constructor(public navCtrl: NavController) {
-
+	public leftCate = [];
+	public rightList = [];
+	public pid = '';
+  constructor(public navCtrl: NavController,public config:ConfigProvider,public httpService:HttpServicesProvider) {
+		this.getLeftData();//左侧分类数据
   }
-
+	getLeftData(){
+		let apiUrl = 'api/pcate';
+		this.httpService.requestData(apiUrl,(data)=>{
+			this.leftCate = data.result;
+			this.getRightData(this.leftCate[0]['_id'])
+		});
+	}
+	
+	getRightData(pid){
+		let apiUrl = 'api/pcate?pid='+pid;
+		this.httpService.requestData(apiUrl,(data)=>{
+			this.rightList = data.result;
+			console.log(data.result);
+		});
+	}
+	
 }
